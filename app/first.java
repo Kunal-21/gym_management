f5b25fa7796d498eb0964011111eadb6c1ebe521
Kunal-21 <80700510+Kunal-21@users.noreply.github.com>
2023-07-04 22:40:59 +0530

    Add files via upload

---
+++ app/first.java
@@ -0,0 +1,10 @@
+import java.applet.Applet;
+import java.applet.*;
+import java.awt.*;
+
+public class first extends Applet {
+
+    public void paint(Graphics g) {
+        g.drawString("welcome", 150, 150);
+    }
+}
